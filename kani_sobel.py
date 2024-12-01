dd55c9386ea8e432c148be35fc19ac86d74607ca
Alexey Vorobyev <a.vorobyev@winsolutions.ru>
2024-12-02 00:13:15 +0300

    Добавлены вариации трешхолда для канни

--- kani_sobel.py
+++ kani_sobel.py
@@ -21,25 +21,30 @@ images = [
 ]
 
 deviations = [
-    100, 10, 1, 0.1
+    10, 1, 0.1
+]
+
+threshold_dividers = [
+    (40, 2), (20, 5), (15, 10)
 ]
 
 for deviation in deviations:
-    kaniAlgo = KaniAlgorythm(
-        image_size=(500, 500),
-        image_show_list=[],
-        kernel_size=7,
-        deviation=deviation,
-        threshold_dividers=(20, 5),
-    )
-
-    for image in images:
-        processed_image, _ = kaniAlgo.process_image_with_return(image["path"])
-
-        cv2.imwrite(
-            f"resources/kani_sobel/{image["name"]}_gauss_{deviation}.png",
-            processed_image
+    for threshold_divider in threshold_dividers:
+        kaniAlgo = KaniAlgorythm(
+            image_size=(500, 500),
+            image_show_list=[],
+            kernel_size=7,
+            deviation=deviation,
+            threshold_dividers=threshold_divider,
         )
+
+        for image in images:
+            processed_image, _ = kaniAlgo.process_image_with_return(image["path"])
+
+            cv2.imwrite(
+                f"resources/kani_sobel/{image["name"]}_gauss_{deviation}_threshold_{threshold_divider[0]}_{threshold_divider[1]}.png",
+                processed_image
+            )
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
